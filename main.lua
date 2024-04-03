ba848b808d130b49d35c1140935855ad3c9e9fa0
Miguel <39136142+LoveBetween@users.noreply.github.com>
2024-04-03 22:52:33 +0200

    sin

--- main.lua
+++ main.lua
@@ -1,32 +1,89 @@
--- RoRR Randomizer v1.0.2
+-- RoRR Randomizer v1.0.3
 -- SmoothSpatula
 
 Toml = require("tomlHelper")
 
 -- ========== Parameters ==========
 
-local max_artis = 14
-local max_skills = 4
+local MAX_ARTI = 14
+local MAX_SKILL = 4
+local is_init = false
+local params = {}
 
-local default_params = {
-    randomize_character = true,
-    randomize_skills = true,
-    randomize_artifacts = true,
+function late_init()   
+    params = Toml.load_cfg(_ENV["!guid"])
+    
+    if not params then
+        local default_params = {
+            randomize_character = true,
+            randomize_skills = true,
+            randomize_artifacts = true,
+        
+            min_skill = 1, --skill 0 is no skill
+            max_skill = 143, --max skill id 205, max survivor skill id 143
+            nb_skill = 4, -- number of skills to roll
+        
+            min_arti = 1, --min artifact  is id 1
+            max_arti = 14, --max artifact is id 14
+            nb_arti = 4,  -- number of artifacts to roll
+            skill = get_skills(1,143)
+        }
+        Toml.save_cfg(_ENV["!guid"], default_params)
+        params = default_params
+    end
+    --[[ Sanity check
+    for i = params['min_skill'] , params['max_skill'] do
+        print(i.." "..params['skill'][i..''].name.."  "..tostring(params['skill'][i..''].enabled))
+    end 
+    ]]
+    -- add skill enable gui
+    
+    gui.add_to_menu_bar(function()
+        for i = params['min_skill'], params['max_skill'] do
+            local c = "  " 
+            if params['skill'][tostring(i)].enabled then c = "v" end
+            if ImGui.Button("["..c.."]  "..params['skill'][i..''].name) then
+                params['skill'][i..''].enabled = not params['skill'][i..''].enabled
+                Toml.save_cfg(_ENV["!guid"], params)
+            end
+        end
+    end)
+    
+    
+end
 
-    min_skill = 1, --skill 0 is no skill
-    max_skill = 142, --max skill id 205, max survivor skill id 142
-    nb_skill = 4, -- number of skills to roll
+-- ========== Utils ==========
 
-    min_arti = 1, --min artifact  is id 1
-    max_arti = 14, --max artifact is id 14
-    nb_arti = 4 -- number of artifacts to roll
-}
+-- create random_array
+function get_rand_list(min_id, max_id, n)
+    local ar = {}
+    local count = 0
+    -- get all enabled skills
+    for i = params['min_skill'], params['max_skill'] do
+        if params['skill'][i..''].enabled then
+            count = count+1
+            ar[count] = i
+        end
+    end
+    -- fast random pick
+    local rnd_ar = {}
+    for i = 1, n do 
+        local rnd_nb = math.random(1, count)
+        rnd_ar[i] = ar[rnd_nb]
+        ar[rnd_nb] = ar[count]
+        count = count-1
+    end
+    return rnd_ar
+end
 
-local params = Toml.load_cfg(_ENV["!guid"])
-
-if not params then
-   Toml.save_cfg(_ENV["!guid"], default_params)
-   params = default_params
+function get_skills(min_id, max_id)
+    local skills = gm.variable_global_get("class_skill")
+    local tab = {}
+    for i = min_id , max_id do
+        -- I should be executed on the spot for doing this but lua/toml gave me no choice
+        tab[i..'']= {name =  skills[i][2], enabled = true}
+    end 
+    return tab
 end
 
 -- ========== ImGui ==========
@@ -54,6 +111,7 @@ gui.add_to_menu_bar(function()
     end
 end)
 
+
 -- set values
 --[[ still buggy
 gui.add_to_menu_bar(function()
@@ -64,32 +122,22 @@ gui.add_to_menu_bar(function()
     end
 end)
 ]]
+
 gui.add_to_menu_bar(function()
-    local new_value, isChanged = ImGui.InputInt("Set of random artifacts", params['nb_arti'], 1, 2, 0)
-    if isChanged and new_value<=max_artis and new_value >= 0 then
+    local new_value, isChanged = ImGui.InputInt("Set of random artifacts", params['nb_arti'], 1, 2)
+    if isChanged and new_value <= MAX_ARTI and new_value >= 0 then
         params ['nb_arti'] = new_value
         Toml.save_cfg(_ENV["!guid"], params)
     end
 end)
 
--- ========== Utils ==========
+-- ========== Late Init ==========
 
-function array_shuffle(x)
-	for i = #x, 2, -1 do
-		local j = math.random(i)
-		x[i], x[j] = x[j], x[i]
-	end
-end
-
--- create random_array
-function get_rand_list(min_id, max_id, n)
-    local ar = {}
-    for i = 1, max_id-min_id+1 do 
-      ar[i] = i+min_id-1
-    end
-    array_shuffle(ar)
-    return ar
-end
+gm.post_script_hook(gm.constants.stage_load_room, function(self, other, result, args)
+    if not is_init then 
+        is_init = true
+        late_init() end
+end)
 
 -- ========== Main ==========
 
@@ -108,7 +156,7 @@ function set_random_char()
                 local rnd_skills = get_rand_list(params['min_skill'], params['max_skill'], params['nb_skill'])
                 for i = 1, params['nb_skill'] do
                     gm.actor_skill_set(inst, i-1, rnd_skills[i])
-		    log.info("skill "..i.." : id = "..rnd_skills[i])
+                    log.info("skill "..i.." : id = "..rnd_skills[i])
                 end
             end
             --set random artifacts
